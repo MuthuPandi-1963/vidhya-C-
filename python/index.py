a3d87b0ac07745d05ddf2b5a58b6b83a428fd933
MuthuPandi-1963 <muthupandir74738@gmail.com>
2025-11-05 17:59:39 +0530

    done

---
+++ python/index.py
@@ -0,0 +1,89 @@
+# print("Hello world")
+# print("Hello world")
+# print("Hello world")
+# print("Hello world")
+# print("Hello world")
+# print("Hello world")
+
+
+# x = 10
+# print(x)
+# print("x")
+# print(type(x))
+
+# y=12.5
+# print(type(y))
+
+# s = "hii"
+# r = 'welcome'
+
+# print(s,r)
+# print(type(s),type(r))
+
+
+# isActive = True
+# isAlive = False
+
+# print(type(isActive),type(isAlive))
+
+# c = None
+
+# print(type(c))
+
+# name = input("Enter a name :")
+
+# print("entered name is ", name)
+
+
+# a = int(input(" enter a number A :"))
+# b = int(input(" enter a number B :"))
+# print(a+b)
+
+# m = 100
+
+# print(type(m))
+
+# m = str(m)
+# print(type(m))
+
+
+# # +,-,*,/,%
+# # // , **
+
+# print(10/3)
+# print(10//3)
+
+# print(2**4)
+
+# # <,>,<=,>=,==,!=
+
+# # += ,-=,*=,/=,//=,%=,**=
+
+# # ++ , -- , && , ||
+
+# print(True and True)
+# print(False and True)
+# print(True and False)
+# print(False and False)
+
+# print(" ")
+
+# print(True or True)
+# print(False or True)
+# print(True or False)
+# print(False or False)
+
+# & , | , 
+
+if(False):
+    print("hello")
+    print("condition is true ")
+else:
+    print("condition is false ")
+
+# start,end,step
+for i in range(1,10,3):
+    print(i)
+
+for i in range(10,10,3):
+    print(i)
