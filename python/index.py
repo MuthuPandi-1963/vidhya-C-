7c83d3ba3429b14220066793f60b8041044f0a55
MuthuPandi-1963 <muthupandir74738@gmail.com>
2025-11-14 18:00:09 +0530

    done

--- python/index.py
+++ python/index.py
@@ -75,15 +75,166 @@
 
 # & , | , 
 
-if(False):
-    print("hello")
-    print("condition is true ")
-else:
-    print("condition is false ")
+# if(False):
+#     print("hello")
+#     print("condition is true ")
+# else:
+#     print("condition is false ")
 
-# start,end,step
-for i in range(1,10,3):
-    print(i)
+# # start,end,step
+# for i in range(1,10,3):
+#     print(i)
 
-for i in range(10,10,3):
-    print(i)
+# for i in range(10,1,-1):
+#     print(i)
+
+# for i in range(20,2,-2):
+#     print(i)
+
+# i=10
+# while i>=0:
+#     print("hello",i)
+#     i-=1
+# print(i)
+
+
+# tables 8,12
+
+# 1*8=8
+
+# void add(int a,int b){}
+# keyword parameters
+# def add(a,b):
+#     return a+b
+
+# print(add(10,15))
+
+# # default parameters
+# def greet(name = "guest"):
+#     print("Hello",name)
+
+# greet()
+# greet("ram")
+
+
+# s = "welcome"
+# print(s.capitalize())
+# print(s.upper())
+# print(s.lower())
+# print(s.count("e"))
+
+# # substring
+# print(s[2])
+# print(s[0:3])
+# # start,end,step
+# print(s[1:4])
+# print(s[1:6:2])
+
+# print(s[2:])
+# print(s[:5])
+
+
+
+# nums = [1,2,3,4,5,6]
+# print(nums)
+# print(nums[4])
+
+# nums[3] = 40
+# print(nums)
+
+# nums.append(60)
+
+# print(nums)
+
+# nums.pop()
+# print(nums)
+
+# nums.insert(3,10)
+# print(nums)
+# nums.append(1)
+# nums.append(1)
+# print(nums)
+# print(nums.count(1))
+# nums.remove(40)
+# print(nums)
+
+# nums.reverse()
+# print(nums)
+
+# nums.sort(reverse=True)
+
+# print(nums)
+
+# fruits = ["apple","orange","banana"]
+# newArr = nums + fruits
+# print(newArr,nums,fruits)
+
+# nums.extend(fruits)
+
+# print(nums)
+
+# a=10
+# b=20
+# c=a+b
+
+# a+=1
+
+# t = (1,2,3,4,5)
+# print(t[1])
+
+# val = {1,2,3,4,5,1,2,3}
+
+# print(val)
+
+# val1 = {10,4,5,1,2,0}
+# print(val1)
+
+# A` U B`
+# A N B
+# print(val.intersection(val1))
+
+# print(val.difference(val1))
+
+# print(val - val1)
+
+# print(val.symmetric_difference(val1))
+# print(val.union(val1))
+# print(val)
+
+
+# dict => key:value 
+# li = [100,80,90,75,85]
+
+# marks = {
+#     # "tamil" : 100,
+#     "e" : 98,
+#     "m" : 89,
+#     "tamil" : 85
+# }
+# print(marks)
+# # unique , access -> key 
+
+# print(marks["tamil"])
+
+# marks["m"] = "hello"
+# print(marks)
+# # marks.pop("e")
+# # marks.popitem()
+# print(marks)
+# print(marks.keys())
+# print(marks.values())
+# print(marks.items())
+
+# print(marks.get("r"))
+# print(marks["r"])
+
+
+map
+filter
+reversed
+sorted
+
+list , dict = 0
+"comprehension"
+
+"https://chatgpt.com/share/69171f72-a25c-800c-af23-60b8e247734c"
